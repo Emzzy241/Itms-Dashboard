1218aa3a2c6350cb9abe0891a0ab2d0187c15bee
Emzzy241 <emzzyoluwole@gmail.com>
2026-05-16 08:32:29 +0100

    fix api key leak with environment variables

--- src/FirebaseConfig.js
+++ src/FirebaseConfig.js
@@ -2,14 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAM12OZOQaJRQpinaDaVD_A7B-dFZRluMs",
-  authDomain: "itms-project-43973.firebaseapp.com",
-  projectId: "itms-project-43973",
-  storageBucket: "itms-project-43973.firebasestorage.app",
-  messagingSenderId: "495788562409",
-  appId: "1:495788562409:web:690bf873a27f061419dd78",
-  measurementId: "G-9MTENGZN2G"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
+console.log(import.meta.env.VITE_FIREBASE_API_KEY);
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
