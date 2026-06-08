490c962ec0597ad5da2ea15a9e93d5622cc92a3a
Emzzy241 <emzzyoluwole@gmail.com>
2026-06-08 22:13:36 +0100

    update firebase config file

--- src/FirebaseConfig.js
+++ src/FirebaseConfig.js
@@ -10,7 +10,7 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
-console.log(import.meta.env.VITE_FIREBASE_API_KEY);
+// console.log(import.meta.env.VITE_FIREBASE_API_KEY);
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
